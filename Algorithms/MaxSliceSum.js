9f8044c6e5958e66555a19a188072209ff747285
Andres Dorado <andres.dorado90@gmail.com>
2021-12-07 18:39:15 +0100

    3 new algorithms for slicing practice

---
+++ Algorithms/MaxSliceSum.js
@@ -0,0 +1,50 @@
+/**
+ A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
+
+Write a function:
+
+function solution(A);
+
+that, given an array A consisting of N integers, returns the maximum sum of any slice of A.
+
+For example, given array A such that:
+
+A[0] = 3  A[1] = 2  A[2] = -6
+A[3] = 4  A[4] = 0
+the function should return 5 because:
+
+(3, 4) is a slice of A that has sum 4,
+(2, 2) is a slice of A that has sum −6,
+(0, 1) is a slice of A that has sum 5,
+no other slice of A has sum greater than (0, 1).
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..1,000,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000];
+the result will be an integer within the range [−2,147,483,648..2,147,483,647].
+ */
+const solution = (A) => {
+    let max = -Number.MAX_SAFE_INTEGER;
+    const N = A.length;
+    for (const i in A) {
+      let array = [];
+      A.slice(i).reduce((a, c) => {
+        a = a + c;
+        array.push(a);
+        return a;
+      }, 0);
+      max = Math.max(...array, max);
+    }
+    return max;
+  };
+  
+  const solutions = [
+    solution([3, 2, -6, 4, 0]),
+    solution([31, -2, -6, 4, 0]),
+    solution([-31, 2, -6, 4, 0]),
+    solution([-10])
+  ];
+
+  for(const s of solutions) {
+    console.log(s);
+  }
--- ex1.js
+++ ex1.js
@@ -1,38 +1,48 @@
-function solution(S) {
-  // write your code in JavaScript (Node.js 8.9.4)
-  // normalize S
-  S = S.toLowerCase();
-  // important chars
-  const array = S.split("");
+function solution(text) {
+  let times = 0;
+  const seed = "balon";
 
-  // check if  the chars exist at least one
+  // preprocessing the text
+  try {
+    text = text.toString().trim();
+    text = text.toLowerCase();
+    console.log(text);
+  } catch (error) {
+    console.error(error);
+  }
 
-  // count number of ocurrence of important chars
-  let groups = new Map();
-  array.reduce((a, c) => {
-    if ("balon".includes(c)) {
-      if (a.has(c)) {
-        a.set(c, a.get(c) + 1);
-      } else {
-        a.set(c, 1);
-      }
-    }
-    return a;
-  }, groups);
-  if (
-    groups.has("b") &&
-    groups.has("a") &&
-    groups.has("l") &&
-    groups.has("o") &&
-    groups.has("n")
-  ) {
-    const min_ban = Math.min(groups.get("b"), groups.get("a"), groups.get("l"));
-    const min_lo = Math.min(groups.get("l"), groups.get("o"));
-
-    return min_lo > min_ban ? min_ban : min_lo / 2;
-  } else {
+  // convert into array
+  const array = text.split("").filter((x) => seed.includes(x));
+  // if not all the chars exist
+  if (seed.split("").some((x) => !array.includes(x))) {
     return 0;
   }
+  console.debug(array);
+
+  // group by chars
+  const groups = array.reduce((a, c) => {
+    a[c] = a[c] ? a[c] + 1 : 1;
+    return a;
+  }, {});
+  console.debug(groups);
+
+  // the chars occurrences
+  const one_oc = ["b", "a", "n"]; //once
+  const two_oc = ["l", "o"]; //twice
+
+  let min_one = Math.min(...one_oc.map((x) => groups[x]));
+  let min_two = Math.min(...two_oc.map((x) => groups[x]));
+
+  console.debug(min_one, min_two);
+
+  if (min_two < 2) return 0;
+  while (min_one > 0 && min_two > 1) {
+    times += 1;
+    min_one -= 1;
+    min_two -= 2;
+  }
+  return times;
 }
 console.log(solution("BAOOLLNNOLOLGBAX"));
 console.log(solution("BANBANLLOOX"));
+console.log(solution("BBBAaOOLLNnnOLOLGBAX"));
