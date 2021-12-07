80293a55b507c252299b9c14480d507695c6e891
Andres Dorado <andres.dorado90@gmail.com>
2021-12-07 20:00:04 +0100

    Use of lodash throttle

--- ex2.js
+++ ex2.js
@@ -1,4 +1,10 @@
-const handleJSON = (jsonString, saveToFile = null) => {
+const _ = require("lodash");
+let data = {};
+const controller = _.throttle(saveToFile, 1000, {
+  trailing: true
+});
+
+const handleJSON = (jsonString) => {
   // Feel free to use console.log() for debugging purposes
   // console.log(jsonString)
   // Normalize strings
@@ -6,24 +12,31 @@ const handleJSON = (jsonString, saveToFile = null) => {
 
   jsonObject.username = normalize(jsonObject.username);
   jsonObject.filename = normalize(jsonObject.filename);
+  data = jsonObject;
 
   // Here is an example usage of saveToFile function
-  // saveToFile("username", "filename", `{"good":"luck"}`)n
-  return jsonObject;
+  controller();
+  return aja;
 };
 
 const normalize = (text) => {
-  text = text.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+  text = text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   text = text.trim();
   text = text.replace(" ", "_");
   return text;
 };
 
+function saveToFile() {
+  console.log(`Saving ${data.username}/${data.filename}`);
+}
+
 const aja = { username: "señor Gonzales", filename: "bÜk.txt", author: "adam" };
 const aja2 = {
   username: "señor Andrés ",
   filename: "ADA f.txt  ",
-  author: "adam ",
+  author: "adam "
 };
 console.log(handleJSON(aja));
-console.log(handleJSON(aja2));
+setTimeout(() => {
+  console.log(handleJSON(aja2));
+}, 2000);
